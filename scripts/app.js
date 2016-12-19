ac9ddd4764a6659ca7fad4d7b886faa552365d72
joshgressman <joshgressman@gmail.com>
2016-12-19 13:08:56 -0600

    added gallery images and jquery

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function(){
+  console.log("Doc ready working");
+
+  $('#image1').on('click', function (){
+    $('#galleryDescription').show();
+  }, function () {
+    $('#galleryDescription').hide();
+  }
+
+);
+
+  ///////**********END OF DOC READY FUNCTION**********//////////
+});
--- templates/gallery.php
+++ templates/gallery.php
@@ -1,14 +1,32 @@
 <?php include "templates/header.php"; ?>
 
   <div class="row" id="theSection">
-    <div class="col-md-3" id="galleryImage">1</div>
-    <div class="col-md-3" id="galleryImage">2</div>
-    <div class="col-md-3" id="galleryImage">3</div>
-    <div class="col-md-3" id="galleryImage">3</div>
+      <div class="col-md-4" id="galleryImage">
+        <img id="image1" src="../img/das.png" alt="Gallery Picture" style="width:100%;height:100%; opacity:0.4;">
+      </div>
+      <div class="col-md-4" id="galleryImage">
+        <img id="image2" src="../img/svc.gif" alt="Gallery Picture" style="width:100%;height:100%;">
+      </div>
+      <div class="col-md-4" id="galleryImage">
+        <img id="image3" src="../img/vip.png" alt="Gallery Picture" style="width:100%;height:100%;">
+      </div>
   </div>
   <div class="row" id="theSection">
-    <div class="col-md-3" id="galleryImage">4</div>
-    <div class="col-md-3" id="galleryImage">5</div>
-    <div class="col-md-3" id="galleryImage">6</div>
-    <div class="col-md-3" id="galleryImage">6</div>
+      <div class="col-md-4" id="galleryImage">
+        <img id="image4" src="../img/store.png" alt="Gallery Picture" style="width:100%;height:100%;">
+      </div>
+      <div class="col-md-4" id="galleryImage">
+        <img id="image5" src="../img/gymsMe.png" alt="Gallery Picture" style="width:100%;height:100%;">
+      </div>
+      <div class="col-md-4" id="galleryImage">
+          <img id="image6" src="../img/landing.png" alt="Gallery Picture" style="width:100%;height:100%;">
+      </div>
+
+    </div>
+  <div class="row" id="galleryDescription">
+    <div class="col-md-4">
+      <p>
+        Some Info
+      </p>
+    </div>
   </div>
